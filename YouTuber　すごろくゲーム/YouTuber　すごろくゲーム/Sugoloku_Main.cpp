a6d437d3214559b9bc2a1c54a0ed941ebfab7248
郁弥 河合 <h1201819034@hiratagakuen.onmicrosoft.com>
2020-07-02 15:32:53 +0900

    スクロール調整中

--- YouTuber　すごろくゲーム/YouTuber　すごろくゲーム/Sugoloku_Main.cpp
+++ YouTuber　すごろくゲーム/YouTuber　すごろくゲーム/Sugoloku_Main.cpp
@@ -3,25 +3,42 @@
 #include "Header.h"
 
 #define MAP_SIZE	50 //マップチップ一つのドットサイズ
-#define MAP_WIDTH	16 //マップの幅
-#define MAP_HEIGHT	12 //マップの縦長さ
+#define MAP_WIDTH	20 //マップの幅
+#define MAP_HEIGHT	20 //マップの縦長さ
+
+#define MOVE_FRAME	32 //移動にかけるフレーム数
 
 // マップのデータ(16マス×12マス)(0 = 描画マス、1 = 壁)
 int MapData[MAP_HEIGHT][MAP_WIDTH] =
 {
-	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } ,
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 } ,
-	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 } ,
-	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 } ,
-	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } ,
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } ,
+	{ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 } ,
+	{ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1 } ,
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 } ,
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 } ,
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1 } ,
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 } ,
+	{ 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 } ,
+	{ 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1 } ,
+	{ 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 } ,
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } ,
 };
+
+//移動しているかどうかのフラグ( 0:停止中  1:移動中 )
+int Move;
+//各方向に移動する量
+int MoveX, MoveY;
+//移動し始めてから何フレーム経過したかを保持する変数
+int MoveCounter;
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
@@ -53,6 +70,8 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	int squares_img1 = LoadGraph("image\\マス.png");
 
 	int i, j;
+	int Key;
+	int ScrollX, ScrollY;
 
 	//画像の位置情報
 	float x, y;
@@ -61,8 +80,8 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	//画像の移動ベクトル最大値
 	float vx_max, vy_max;
 	//初期化 
-	x = 50.0f;
-	y = 5.0f;
+	x = 350.0f;
+	y = 275.0f;
 	vx = 0.0f;
 	vy = 0.0f;
 	vx_max = 0.0f;
@@ -82,82 +101,132 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	int rect_x = 0;
 	int rect_y = 0;
 
+	//最初は停止中(0)にしておく
+	Move = 0;
+
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
-		//方向設定
-		//右
-		if (CheckHitKey(KEY_INPUT_RIGHT)) {
-			LR_flg = 1;
-			Direction_of_Travel_num = 0;
-		}
-		//左
-		else if (CheckHitKey(KEY_INPUT_LEFT)) {
-			LR_flg = 0;
-			Direction_of_Travel_num = 1;
-		}
-		//上
-		else if (CheckHitKey(KEY_INPUT_UP)) {
-			UD_flg = 1;
-			Direction_of_Travel_num = 2;
-		}
-		//下
-		else if (CheckHitKey(KEY_INPUT_DOWN)) {
-			UD_flg = 0;
-			Direction_of_Travel_num = 3;
+		
+			//方向設定
+			//右
+			if (CheckHitKey(KEY_INPUT_RIGHT)) {
+				LR_flg = 1;
+				Direction_of_Travel_num = 0;
+			}
+			//左
+			else if (CheckHitKey(KEY_INPUT_LEFT)) {
+				LR_flg = 0;
+				Direction_of_Travel_num = 1;
+			}
+			//上
+			else if (CheckHitKey(KEY_INPUT_UP)) {
+				UD_flg = 1;
+				Direction_of_Travel_num = 2;
+			}
+			//下
+			else if (CheckHitKey(KEY_INPUT_DOWN)) {
+				UD_flg = 0;
+				Direction_of_Travel_num = 3;
+			}
+
+			// 移動中ではない場合キー入力を受け付ける
+			if (Move == 0)
+			{
+
+			//Enterで前進フラグをtrue、1P移動距離を設定
+			if (CheckHitKey(KEY_INPUT_RETURN) && Forward_flg == false)
+			{
+				if (Direction_of_Travel_num == 0)
+				{
+					//vx = x + 50; //右
+					//vx_max = 2.0f;
+					Move = 1;
+					MoveX = 1;
+					MoveY = 0;
+				}
+				else if (Direction_of_Travel_num == 1)
+				{
+					//vx = x - 50;//左
+					//vx_max = -2.0f;
+					Move = 1;
+					MoveX = -1;
+					MoveY = 0;
+				}
+				else if (Direction_of_Travel_num == 2)
+				{
+					//vy = y - 50;//上
+					//vy_max = -2.0f;
+					Move = 1;
+					MoveX = 0;
+					MoveY = -1;
+				}
+				else if (Direction_of_Travel_num == 3)
+				{
+					//vy = y + 50;//下
+					//vy_max = 2.0f;
+					Move = 1;
+					MoveX = 0;
+					MoveY = 1;
+				}
+				Forward_flg = true;
+			}
+
+			//停止中は画面のスクロールは行わない
+			ScrollX = 0;
+			ScrollY = 0;
 		}
 
-		//Enterで前進フラグをtrue、1P移動距離を設定
-		if (CheckHitKey(KEY_INPUT_RETURN) && Forward_flg == false)
+		// 移動中の場合は移動処理を行う
+		if (Move == 1)
 		{
-			if (Direction_of_Travel_num == 0)
+			MoveCounter++;
+			//設定した移動距離までに移動
+			//左右
+			//if (vx != x)
+			//{
+			//	x += vx_max;
+			//}
+			//上下
+			//else if (vy != y)
+			//{
+			//	y += vy_max;
+			//}
+			//設定した移動距離に到着すると停止(Enterを押しっぱなしによる連続移動を止める処理付き)
+			if (CheckHitKey(KEY_INPUT_RETURN) == false)
 			{
-				vx = x + 50; //右
-				vx_max = 2.0f;
+				//初期化 
+				if (vx == x)
+				{
+					vx_max = 0.0f;
+				}
+				else if (vy == y)
+				{
+					vy_max = 0.0f;
+				}
+				Forward_flg = false;
 			}
-			else if (Direction_of_Travel_num == 1)
+
+			// 移動処理が終了したら停止中にする
+			if (MoveCounter == MOVE_FRAME)
 			{
-				vx = x - 50;//左
-				vx_max = -2.0f;
+				// プレイヤーの位置を変更する
+				x += MoveX;
+				y += MoveY;
+
+				// 停止中は画面のスクロールは行わない
+				//ScrollX = 0;
+				//ScrollY = 0;
+
+				Move = 0;
 			}
-			else if (Direction_of_Travel_num == 2)
+			else
 			{
-				vy = y - 50;//上
-				vy_max = -2.0f;
+				// 経過時間からスクロール量を算出する
+				ScrollX = -(MoveX * MAP_SIZE * MoveCounter / MOVE_FRAME);
+				ScrollY = -(MoveY * MAP_SIZE * MoveCounter / MOVE_FRAME);
 			}
-			else if (Direction_of_Travel_num == 3)
-			{
-				vy = y + 50;//下
-				vy_max = 2.0f;
-			}
-			Forward_flg = true;
 		}
-		
-		//設定した移動距離までに移動
-		//左右
-		if (vx != x)
-		{
-			x += vx_max;
-		}
-		//上下
-		else if (vy != y)
-		{
-			y += vy_max;
-		}	
-		//設定した移動距離に到着すると停止(Enterを押しっぱなしによる連続移動を止める処理付き)
-		if (CheckHitKey(KEY_INPUT_RETURN) == false)
-		{
-			//初期化 
-			if (vx == x)
-			{
-				vx_max = 0.0f;
-			}
-			else if (vy == y)
-			{
-				vy_max = 0.0f;
-			}
-			Forward_flg = false;
-		}
-		
+
 		ClearDrawScreen(); //画像クリア
 
 		//マップを描く
@@ -168,7 +237,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 				if (MapData[i][j] == 0)
 				{
 					DrawRectGraphF(
-						j * MAP_SIZE, i * MAP_SIZE,  //描画位置
+						j * MAP_SIZE + ScrollX, i * MAP_SIZE + ScrollY,  //描画位置
 						0, 0, //切り取り開始位置
 						50, 50, //切り取るサイズ
 						squares_img1,  //切り取る元画像
@@ -211,8 +280,8 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 		DrawBox(0, 0, 800, 50, GetColor(0, 0, 0), TRUE);//四角形を描画
 		//文字を描画する
 		//登録者数
-		DrawFormatString(7, 7, GetColor(255, 255, 255), "1Pチャンネル登録者数：1000人(仮)");
-		DrawFormatString(7, 29, GetColor(255, 255, 255), "2Pチャンネル登録者数：1000人(仮)");
+		DrawFormatString(7, 7, GetColor(255, 255, 255), "1Pチャンネル登録者数：1000人");
+		DrawFormatString(7, 29, GetColor(255, 255, 255), "2Pチャンネル登録者数：1000人");
 		//ゴールまでの歩数
 		DrawFormatString(300, 20, GetColor(255, 255, 255), "ゴールまで");
 		DrawFormatString(395, 7, GetColor(255, 255, 255), "1P：あと30歩");
@@ -222,16 +291,12 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 		DrawFormatString(625, 7, GetColor(255, 0, 0), "1位：1P");
 		DrawFormatString(625, 29, GetColor(255, 255, 255), "2位：2P");
 
-		int x_c1 = 55, y_c1 = 0;
+		int x_c1 = 355, y_c1 = 350;
 		//文字を描画する
-		//DrawFormatString(x_c1, y_c1, GetColor(255, 255, 0), "START");
-		int x_c2 = 505, y_c2 = 300;
+		DrawFormatString(x_c1 + ScrollX, y_c1 + ScrollY, GetColor(255, 255, 0), "START");
+		int x_c2 = 453, y_c2 = 185;
 		//文字を描画する
-		DrawFormatString(x_c2, y_c2, GetColor(255, 0, 0), "GOAL！");
-
-		//x_c = x_c; // x座標を2加算
-		//y_c = y_c; // y座標を1加算
-
+		DrawFormatString(x_c2 + ScrollX, y_c2 + ScrollY, GetColor(255, 0, 0), "GOAL！");
 
 		ScreenFlip(); //バックバッファと切り替え
 
