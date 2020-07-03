f791c9f6f21d3346f8bc1ef7a76c30d86bc7d627
高寺 陸斗 <h1201819020@hiratagakuen.onmicrosoft.com>
2020-07-03 11:06:11 +0900

    マス目移動処理

--- YouTuber　すごろくゲーム/YouTuber　すごろくゲーム/Sugoloku_Main.cpp
+++ YouTuber　すごろくゲーム/YouTuber　すごろくゲーム/Sugoloku_Main.cpp
@@ -2,6 +2,10 @@
 #include "DxLib.h"
 #include "Header.h"
 
+#include <time.h>
+
+#define RECR_MAX 200
+
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
@@ -31,84 +35,173 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 	//画像読み込み
 	int image = LoadGraph("image\\Frisk.png");
 	int back_img1 = LoadGraph("image\\背景テスト用.png");
+	int Rou_image = LoadGraph("image\\スロット.png");
 	DrawGraph(0, 0, back_img1, TRUE);
 
-	//画像の位置情報
-	float x, y;
-	//画像の移動距離情報
-	float vx, vy;
-	//画像の移動ベクトル最大値
-	float vx_max, vy_max;
-	//初期化 
-	x = 0.0f;
-	y = 536.0f;
-	vx = 0.0f;
-	vy = 0.0f;
-	vx_max = 0.0f;
-	vy_max = 0.0f;
-	//左右向きフラグ
-	bool LR_flg = 1;
-	//上下向きフラグ
-	bool UD_flg = 0;
-	//進行方向管理変数
-	int Direction_of_Travel_num = 0; //0:右 1:左 2:上 3:下
-	//前進フラグ
-	bool Forward_flg = false;
+	//プレイヤー
+		//画像の位置情報
+		float x, y;
+		//画像の移動距離情報
+		float vx, vy;
+		//画像の移動ベクトル最大値
+		float vx_max, vy_max;
+		//初期化 
+		x = 0.0f;
+		y = 536.0f;
+		vx = 0.0f;
+		vy = 0.0f;
+		vx_max = 0.0f;
+		vy_max = 0.0f;
+		//左右向きフラグ
+		bool LR_flg = 1;
+		//上下向きフラグ
+		bool UD_flg = 0;
+		//進行方向管理変数
+		int Direction_of_Travel_num = 0; //0:右 1:左 2:上 3:下
 
-	//アニメーション用カウント
-	int anim_cnt = 0;
-	//切り取り位置
-	int rect_x = 0;
-	int rect_y = 0;
+		//アニメーション用カウント
+		int anim_cnt = 0;
+		//切り取り位置
+		int rect_x = 0;
+		int rect_y = 0;
+
+	//ルーレット
+		//画像の位置情報
+		float Rou_x, Rou_y;
+		//初期化 
+		Rou_x = 300.0f;
+		Rou_y = 200.0f;
+		//左右向きフラグ
+		bool Rou_LR_flg = 0;
+		//ボタン確認(ルーレット用)
+		bool Roulette_Enter_Bottan = false;
+		//ルーレット回転
+		bool Roulette_Rotation = false;
+		//ルーレット回転・停止
+		int Roulette = 0;		
+
+		//切り取り位置
+		int Rou_rect_x = 0;
+		int Rou_rect_y = 0;
+
+	//共有
+		//主人公移動開始フラグ
+		bool PlayerMove_Flg = false;
+		//主人公移動回数
+		int PlayerMove_num = 0;
 
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
-		//方向設定
+		//ルーレット処理
+		//Enterでルーレット回転スタート
+		if (CheckHitKey(KEY_INPUT_RETURN) == true && Roulette_Enter_Bottan == false)
+		{
+			if (Roulette == 0)
+			{
+				//ルーレット回転開始
+				Roulette_Rotation = true;
+				PlayerMove_num = 0; //初期化
+				Roulette = 1;
+			}
+			else if (Roulette == 1)
+			{				
+				//ルーレット停止
+				PlayerMove_Flg = true; //主人公移動開始
+				Roulette_Rotation = false; //初期化
+				Roulette = 0; //初期化
+			}
+			Roulette_Enter_Bottan = true;
+		}
+		else if (CheckHitKey(KEY_INPUT_RETURN) == false)
+		{
+			Roulette_Enter_Bottan = false;
+		}
+
+		//ルーレット回転処理
+		if (Roulette_Rotation == true)
+		{
+			//画像切り取り位置変更処理
+			if (Rou_rect_x < 400) {
+				Rou_rect_x += 200;
+			}
+			else {
+				Rou_rect_x = 0; //初期化
+				if (Rou_rect_y < 200) {
+					Rou_rect_y = 200;
+				}
+				else {
+					Rou_rect_y = 0; //初期化
+				}
+			}
+		}
+
+		//移動距離調整処理
+		if (Rou_rect_y < 200)
+		{
+			if (Rou_rect_x == 0){
+				PlayerMove_num = 1;
+			}
+			else if (Rou_rect_x == 200){
+				PlayerMove_num = 2;
+			}
+			else if (Rou_rect_x == 400){
+				PlayerMove_num = 3;
+			}
+		}
+		else if (Rou_rect_y == 200)
+		{
+			if (Rou_rect_x == 0) {
+				PlayerMove_num = 4;
+			}
+			else if (Rou_rect_x == 200){
+				PlayerMove_num = 5;
+			}
+			else if (Rou_rect_x == 400){
+				PlayerMove_num = 6;
+			}
+		}
+
+		//移動方向設定
 		//右
-		if (CheckHitKey(KEY_INPUT_RIGHT)) {
-			LR_flg = 1;
-			Direction_of_Travel_num = 0;
+		if (Direction_of_Travel_num == 0) {
+			LR_flg = 1;			
 		}
 		//左
-		else if (CheckHitKey(KEY_INPUT_LEFT)) {
-			LR_flg = 0;
-			Direction_of_Travel_num = 1;
+		else if (Direction_of_Travel_num == 1) {
+			LR_flg = 0;			
 		}
 		//上
-		else if (CheckHitKey(KEY_INPUT_UP)) {
-			UD_flg = 1;
-			Direction_of_Travel_num = 2;
+		else if (Direction_of_Travel_num == 2) {
+			UD_flg = 1;			
 		}
 		//下
-		else if (CheckHitKey(KEY_INPUT_DOWN)) {
-			UD_flg = 0;
-			Direction_of_Travel_num = 3;
+		else if (Direction_of_Travel_num == 3) {
+			UD_flg = 0;			
 		}
 
 		//Enterで前進フラグをtrue、1P移動距離を設定
-		if (CheckHitKey(KEY_INPUT_RETURN) && Forward_flg == false)
+		if (PlayerMove_Flg == true)
 		{
 			if (Direction_of_Travel_num == 0)
 			{
-				vx = x + 64; //右
+				vx = x + (64 * PlayerMove_num); //右
 				vx_max = 2.0f;
 			}
 			else if (Direction_of_Travel_num == 1)
 			{
-				vx = x - 64;//左
+				vx = x - (64 * PlayerMove_num);//左
 				vx_max = -2.0f;
 			}
 			else if (Direction_of_Travel_num == 2)
 			{
-				vy = y - 64;//上
+				vy = y - (64 * PlayerMove_num);//上
 				vy_max = -2.0f;
 			}
 			else if (Direction_of_Travel_num == 3)
 			{
-				vy = y + 64;//下
+				vy = y + (64 * PlayerMove_num);//下
 				vy_max = 2.0f;
 			}
-			Forward_flg = true;
 		}
 		
 		//設定した移動距離までに移動
@@ -121,9 +214,9 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 		else if (vy != y)
 		{
 			y += vy_max;
-		}	
+		}			
 		//設定した移動距離に到着すると停止(Enterを押しっぱなしによる連続移動を止める処理付き)
-		if (CheckHitKey(KEY_INPUT_RETURN) == false)
+		if (PlayerMove_Flg == true)
 		{
 			//初期化 
 			if (vx == x)
@@ -134,11 +227,12 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 			{
 				vy_max = 0.0f;
 			}
-			Forward_flg = false;
+			PlayerMove_Flg = false;
+			Direction_of_Travel_num = 0;
 		}
 		
 		ClearDrawScreen(); //画像クリア
-		//描画処理
+		//プレイヤー描画処理
 		DrawRectGraphF(
 			x, y,  //描画位置
 			rect_x, rect_y, //切り取り開始位置
@@ -146,6 +240,16 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,
 			image,  //切り取る元画像
 			TRUE, //透過処理フラグ
 			LR_flg //反転処理フラグ
+		);
+
+		//ルーレット描画処理
+		DrawRectGraphF(
+			Rou_x, Rou_y,  //描画位置
+			Rou_rect_x, Rou_rect_y, //切り取り開始位置
+			RECR_MAX, RECR_MAX, //切り取るサイズ
+			Rou_image,  //切り取る元画像
+			TRUE, //透過処理フラグ
+			Rou_LR_flg //反転処理フラグ
 		);
 
 		ScreenFlip(); //バックバッファと切り替え
